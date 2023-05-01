9dda7ecbcd9f1859bdea7dd736b685212c2abe41
na-br-wo <nathan.b.wood@gmail.com>
2023-05-01 07:12:04 -0600

    setting up handlebars

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('./views/main.handlebars')
 })
 
 // Starts the server to begin listening
